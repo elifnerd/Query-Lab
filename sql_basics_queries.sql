951da47239266811a2f14f4e20782ce242774068
JJ Vega <69365848+jjvega86@users.noreply.github.com>
2022-12-05 10:47:08 -0500

    Add files via upload

---
+++ sql_basics_queries.sql
@@ -0,0 +1,10 @@
+-- PROBLEM 1: How many students are in the students table? 
+-- PROBLEM 2: Which student(s) has the highest GPA in the table? 
+-- PROBLEM 3: How many students are located in the state of Virginia? 
+-- PROBLEM 4: How many students have a GPA that is greater than 3.5? 
+-- PROBLEM 5: How many students are NOT on the Honors List? 
+-- PROBLEM 6: Two students have the email domain “narod.ru”. What are the names of those students? 
+-- PROBLEM 7: How many students have a GPA between 2.0 and 3.0? (inclusive) 
+-- PROBLEM 8: What is the average GPA of all students in the students table? 
+-- PROBLEM 9: What is the average GPA of all students in the state of California? 
+-- PROBLEM 10: Which student has the lowest GPA of all students with an address that contains the word “Street”?
